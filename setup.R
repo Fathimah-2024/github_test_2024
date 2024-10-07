927b7f0eb4ffee8f392cf2e494d3893a6fc44235
Fathimah-2024 <fathimah.handayani@colostate.edu>
2024-10-07 09:32:52 -0600

    initial commit

---
+++ setup.R
@@ -0,0 +1,20 @@
+# a script for installing (if needed) and loading packages for this project.
+
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+packageLoad('tidyverse')
+
+# create a string of package names
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'lterdatasampler')
+
+packageLoad(packages)
